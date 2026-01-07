f55545982eb4a10ededae40133f39c60ad255df9
amr <mamad.aminamr@gmail.com>
2026-01-07 11:24:05 +0330

    version0.2.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="icon" />
       </head>
       <body className={` antialiased`}>
         <Header />
